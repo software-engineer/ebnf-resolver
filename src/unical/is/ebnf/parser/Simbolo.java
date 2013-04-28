d605ad2eb9de770b811cdaeef87b9e4e5c1f1d4b
renatomiracco@gmail.com <renatomiracco@gmail.com@f0bcb542-7dc1-b492-4384-751e71bfcf4a>
2013-04-28 20:25:01 +0000

    parser

---
+++ src/unical/is/ebnf/parser/Simbolo.java
@@ -0,0 +1,17 @@
+package unical.is.ebnf.parser;
+
+/**
+ * @author Marilena Paldino
+ * 
+ */
+public enum Simbolo {
+	SUM,
+	DIF,
+	MUL,
+	DIV,
+	VALUE,
+	TONDA_APERTA,
+	TONDA_CHIUSA,
+	CHAR_INVALIDO,
+	EOF
+}
