966feb1db65017a8bbb6427a21e0b69c8d730e5e
renatomiracco@gmail.com <renatomiracco@gmail.com@f0bcb542-7dc1-b492-4384-751e71bfcf4a>
2013-04-29 08:08:14 +0000

    contesto

---
+++ src/unical/is/ebnf/visitor/contesto/Contesto.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package unical.is.ebnf.visitor.contesto;
+
+/**
+ * @author Marilena Paldino
+ */
+public interface Contesto {
+
+	/**
+	 * @param variabile
+	 * @return
+	 */
+	double getValore(String variabile);
+}
